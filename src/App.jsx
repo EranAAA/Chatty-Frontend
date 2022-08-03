1a9282f7d5a19ff0d742680aa330b5acbd28315a
Kiran Kabaso <kirankabaso@gmail.com>
2022-08-03 11:48:32 +0300

    logo and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {LogoBlue} from '../src/services/svg-service.js';
 
 export const App = () => {
    return (
       <div className="App">
          <h1>Hello</h1>
+         <div><LogoBlue/></div>
          
       </div>
    );
