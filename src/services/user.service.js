03bb86814fbf55bea93893a7820e286ba709d233
Kiran Kabaso <kirankabaso@gmail.com>
2022-08-03 15:08:48 +0300

    user data update

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,6 +9,7 @@ function getUsers() {
             _id: 101,
             username: 'ronen',
             fullName: 'ronenberger',
+            email: 'ronenberger@gmail.com',
             friends: [{
                 _id: 102,
                 fullName: 'orgeva'
@@ -19,6 +20,7 @@ function getUsers() {
             _id: 102,
             username: 'or',
             fullName: 'orgeva',
+            email: 'orgeva@gmail.com',
             friends: [{
                 _id: 101,
                 fullName: 'ronenberger'
@@ -29,6 +31,7 @@ function getUsers() {
             _id: 103,
             username: 'noga',
             fullName: 'nogashalev',
+            email: 'nogashalev@gmail.com',
             friends: [
                 {
                     _id: 101,
