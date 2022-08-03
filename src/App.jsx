802218730729199efca60c705b90f6a360cd145c
Kiran Kabaso <kirankabaso@gmail.com>
2022-08-03 12:08:18 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import routes from './routes.js'
+import { Routes, Route } from 'react-router'
 
 export const App = () => {
    return (
       <div className="App">
-         <h1>Hello</h1>
-         
+         <header>
+            <h1>app-header</h1>
+         </header>
+         <main className="routes-container">
+            <Routes>
+               {routes.map(route =>
+                  <Route key={route.path}
+                     exact={true}
+                     element={route.component}
+                     path={route.path} />)}
+            </Routes>
+         </main>
+         <footer>
+            <h1>Footer</h1>
+         </footer>
+
       </div>
    )
 }
---
+++ src/pages/signin-signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SignInUp = () => {
+    return (
+        <div>
+            <h1>Hello from SignInUp</h1>
+        </div>
+    )
+}
